e1f2453b5c9a8ada98440e549b616a48d16278e4
ESCINF <ESCINF@LEN-M83>
2019-08-16 08:59:36 -0600

    :)

--- CalculoPrestamo.cpp
+++ CalculoPrestamo.cpp
@@ -11,44 +11,49 @@
     CalculoPrestamo::CalculoPrestamo(int64_t amt) : monto(amt) {}  
 
     float CalculoPrestamo::obtenerPorcentaje(std::string porcentajeTXT){
-        int a = porcentajeTXT.find("%");
+        int porc = porcentajeTXT.find("%");
         float porcentaje = std::stof(porcentajeTXT);
-            if (a >= 0){
-                porcentaje = std::stof(porcentajeTXT.substr(0,a))/100;
-                return porcentaje;}
-            else return porcentaje;}
+            if (porc >= 0){
+                porcentaje = std::stof(porcentajeTXT.substr(0,porc))/100;
+                return porcentaje;
+            }
+            else return porcentaje;
+    }
     int CalculoPrestamo::calcularTiempoEnMeses(std::string tiempoTXT) {
-        int findano = tiempoTXT.find("A");
-        int findmes = tiempoTXT.find("M");
+        int findAno = tiempoTXT.find("A");
+        int findMes = tiempoTXT.find("M");
         int tiempo = 0;
-        if (findano > 0){
+        if (findAno > 0){
             int ano = 0;
-            ano = std::stoi(tiempoTXT.substr(0,findano));
+            ano = std::stoi(tiempoTXT.substr(0,findAno));
             tiempo = ano * 12;
-            return tiempo;}
+            return tiempo;
+        }
         else
-            tiempo = std::stoi(tiempoTXT.substr(0,findmes));
-            return tiempo;}
+            tiempo = std::stoi(tiempoTXT.substr(0,findMes));
+            return tiempo;
+    }
     float CalculoPrestamo::calcularInteresMensual(float balance, float tasaAnual){
         float tasaMensual = tasaAnual / 12;
         float interes = (tasaMensual * balance);
-        return interes;}
+        return interes;
+    }
 
 
 
- std::string CalculoPrestamo::reporteCalculoPrestamo(std::string tiempoTXT, std::string porcentajeTXT){
-     std::string reporte = "Prestamo por mes: \n";
-     int tiempo = calcularTiempoEnMeses(tiempoTXT);
-     float porcentaje = obtenerPorcentaje(porcentajeTXT);
-     float balance = getMonto();
-     float nuevoBalance = getMonto();
-     float interesMensual = 0;
-     for(int mes = 0; mes < tiempo; ++mes){
-         interesMensual = calcularInteresMensual(nuevoBalance, porcentaje);
-         balance = nuevoBalance;
-         nuevoBalance = balance + interesMensual;
-         std::string mon = std::to_string(balance);
-         reporte = reporte + mon + " \n";
+    std::string CalculoPrestamo::reporteCalculoPrestamo(std::string tiempoTXT, std::string porcentajeTXT){
+        std::string reporte = "Calcular Prestamo: \n";
+        int tiempo = calcularTiempoEnMeses(tiempoTXT);
+        float porcentaje = obtenerPorcentaje(porcentajeTXT);
+        float balance = getMonto();
+        float nuevoBalance = getMonto();
+        float interesMensual = 0;
+        for(int mes = 1; mes <= tiempo; ++mes){
+            interesMensual = calcularInteresMensual(nuevoBalance, porcentaje);
+            balance = nuevoBalance;
+            nuevoBalance = balance + interesMensual;
+            std::string mon = std::to_string(balance);
+            reporte = "Tasa [" + std::to_string(porcentaje) + "], Mes[" + std::to_string(mes) + "], Balance inicial[" +  std::to_string(balance) + "], Balance Nuevo[" +  std::to_string(nuevoBalance) + "], Interes [" +  std::to_string(interesMensual) + "], Balance Nuevo[" + std::to_string(nuevoBalance) + "]\n";
         }
      return reporte;
     }
